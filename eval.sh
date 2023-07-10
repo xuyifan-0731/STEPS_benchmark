0b17d8c98c0a264a2d507c16940686b50762f941
leixy20 <leixy20@mails.tsinghua.edu.cn>
2023-07-10 17:09:53 +0800

    add eval.sh

---
+++ eval.sh
@@ -0,0 +1,11 @@
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/c-eval-val.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/gsm8k_cn.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/gsm8k_en.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/bbh.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/mmlu_zero_shot.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/c-eval-test.yaml
+
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/idiotqa.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/math.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/nqopen.yaml
+python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/trivia_qa.yaml
