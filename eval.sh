04d6ae3c815145783ec074d57d61e6bd9eb858d3
Longin-Yu <longinyh@gmail.com>
2023-07-10 17:37:50 +0800

    eval

--- eval.sh
+++ eval.sh
@@ -1,11 +1,15 @@
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/c-eval-val.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/gsm8k_cn.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/gsm8k_en.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/bbh.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/mmlu_zero_shot.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/c-eval-test.yaml
+AGENT_CONFIG=configs/agents/do_nothing.yaml # Change this into your agent config
 
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/idiotqa.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/math.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/nqopen.yaml
-python evaluate.py --agent configs/agents/do_nothing.yaml --task configs/tasks/full/trivia_qa.yaml
+python evaluate.py \
+    --agent "$AGENT_CONFIG" \
+    --task \
+        configs/tasks/full/c-eval-val.yaml \
+        configs/tasks/full/gsm8k_cn.yaml \
+        configs/tasks/full/gsm8k_en.yaml \
+        configs/tasks/full/bbh.yaml \
+        configs/tasks/full/mmlu_zero_shot.yaml \
+        configs/tasks/full/c-eval-test.yaml \
+        configs/tasks/full/idiotqa.yaml \
+        configs/tasks/full/math.yaml \
+        configs/tasks/full/nqopen.yaml \
+        configs/tasks/full/trivia_qa.yaml
