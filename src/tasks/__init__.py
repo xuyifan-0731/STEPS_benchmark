8a2726b7cf72059c6c5cccd8fbe469ce3fb5018c
Longin-Yu <longinyh@gmail.com>
2023-07-10 16:46:39 +0800

    task

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,8 +1,27 @@
 from .example_task import ExampleTask
 from .humaneval_x import HumanEvalXGenerationTask, HumanEvalXTranslationTask
-from .mbpp import MBPPTask
-from .tool_execution import ToolExecution
-from .single_round_tasks import *
-from .composite_task import CompositeTask
-from .apibench import Apibench
-from .os_interaction import OSInteraction
+
+try:
+    from .mbpp import MBPPTask
+except:
+    print("> [Warning] MBPPTask Import Failed")
+try:
+    from .tool_execution import ToolExecution
+except:
+    print("> [Warning] ToolExecution Import Failed")
+try:
+    from .single_round_tasks import *
+except:
+    print("> [Warning] Single Round Tasks Import Failed")
+try:
+    from .composite_task import CompositeTask
+except:
+    print("> [Warning] CompositeTask Import Failed")
+try:
+    from .apibench import Apibench
+except:
+    print("> [Warning] Apibench Import Failed")
+try:
+    from .os_interaction import OSInteraction
+except:
+    print("> [Warning] OSInteraction Import Failed")
